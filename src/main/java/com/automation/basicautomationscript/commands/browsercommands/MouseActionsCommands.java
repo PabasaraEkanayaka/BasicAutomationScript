d65cc13463d5ca188c9e9a727d26b608b94d6c92
Pabasara Ekanayaka <pabasarasgck@gmail.com>
2025-05-29 10:40:31 +0530

    MouseActionCommands.java added

--- src/main/java/com/automation/basicautomationscript/commands/browsercommands/MouseActionsCommands.java
+++ src/main/java/com/automation/basicautomationscript/commands/browsercommands/MouseActionsCommands.java
@@ -4,10 +4,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class MouseActionsCommands {
     public static void main(String[] args) {
-
+        testDoubleClickAction();
     }
 
     public static void testDoubleClickAction(){
@@ -15,6 +16,12 @@ public class MouseActionsCommands {
         driver.manage().window().maximize();
         driver.get("https://unixpapa.com/js/testmouse.html");
         WebElement clickHereToTest = driver.findElement(By.linkText("click here to test"));
+        Actions actions = new Actions(driver);
+        actions.doubleClick(clickHereToTest).perform();
+
+        WebElement textarea = driver.findElement(By.tagName("textarea"));
+        String value = textarea.getAttribute("value");
+        System.out.println(value);
 
     }
 }
