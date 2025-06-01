fe3248277589790f97d4519d5b47c8e49ccc01dc
Pabasara Ekanayaka <pabasarasgck@gmail.com>
2025-06-01 09:58:50 +0530

    ChainedActionWithCallbackCommand.java added

---
+++ src/main/java/com/automation/basicautomationscript/commands/browsercommands/ChainedActionWithCallbackCommand.java
@@ -0,0 +1,36 @@
+package com.automation.basicautomationscript.commands.browsercommands;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class ChainedActionWithCallbackCommand {
+    public static void main(String[] args) throws InterruptedException {
+        ChainedActionWithCallback();
+    }
+    public static void ChainedActionWithCallback() throws InterruptedException {
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/menu");
+
+        WebElement mainMenu = driver.findElement(By.xpath("//a[text()='Main Item 2']"));
+        WebElement subMenu = driver.findElement(By.xpath("//a[text()='SUB SUB LIST »']"));
+        WebElement subSubItem = driver.findElement(By.xpath("//a[text()='Sub Sub Item 1']"));
+
+        Actions actions = new Actions(driver);
+
+        performActionWithCallback(actions,mainMenu,subMenu,subSubItem);
+        Thread.sleep(3000);
+    }
+
+    private static void performActionWithCallback(Actions actions, WebElement mainMenu, WebElement subMenu, WebElement subSubItem) {
+        actions.moveToElement(mainMenu)
+                .moveToElement(subMenu)
+                .moveToElement(subSubItem)
+                .click()
+                .build()
+                .perform();
+    }
+}
